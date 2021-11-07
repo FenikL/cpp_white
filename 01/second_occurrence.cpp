43f5bccf10c6c24c4f60ba20212a346c2b5a624f
FenikL <fedotov.ia15@physics.msu.ru>
2021-11-08 01:39:07 +0300

    done

--- 01/second_occurrence.cpp
+++ 01/second_occurrence.cpp
@@ -1,4 +1,25 @@
-//
-// Created by IF on 11/8/21.
-//
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main() {
+    string f;
+    int counter = -2;
+
+
+    cin >> f;
+
+    for (int i=0; i<f.size(); ++i){
+        if (f[i] == 'f'){
+            ++counter;
+        }
+        if (counter == 0) {
+            counter = i;
+            break;
+        }
+    }
+    cout << counter;
+
+    return 0;
+}
