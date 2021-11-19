932a77d607aaf3d01ae037e062886fb1248d0c5c
FenikL <fedotov.ia15@physics.msu.ru>
2021-11-19 17:34:05 +0300

    done

--- 02/unique_strings.cpp
+++ 02/unique_strings.cpp
@@ -1,4 +1,18 @@
-//
-// Created by user on 19.11.2021.
-//
+#include <iostream>
+#include <string>
+#include <set>
 
+using namespace std;
+
+int main() {
+    int N;
+    set<string> strings;
+    cin >> N;
+    string s;
+    for (int i=0; i<N; ++i) {
+        cin >> s;
+        strings.insert(s);
+    }
+    cout << strings.size();
+    return 0;
+}
