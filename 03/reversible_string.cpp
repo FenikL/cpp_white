64e877cd233e7611ff9a6e5e193cc19065a7859d
FenikL <fedotov.ia15@physics.msu.ru>
2021-11-24 16:21:29 +0300

    done

---
+++ 03/reversible_string.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class ReversibleString {
+public:
+    ReversibleString() {};
+    ReversibleString(const string& s) {
+       str = s;
+    }
+    void Reverse() {
+        int len = str.size();
+        for (int i = 0; i < (len/2); ++i) {
+            swap(str[i], str[len-1-i]);
+        }
+    }
+    string ToString() const{
+        return str;
+    }
+private:
+    string str;
+};
+
+int main() {
+    ReversibleString s("live");
+    s.Reverse();
+    cout << s.ToString() << endl;
+
+    s.Reverse();
+    const ReversibleString& s_ref = s;
+    string tmp = s_ref.ToString();
+    cout << tmp << endl;
+
+    ReversibleString empty;
+    cout << '"' << empty.ToString() << '"' << endl;
+
+    return 0;
+}
